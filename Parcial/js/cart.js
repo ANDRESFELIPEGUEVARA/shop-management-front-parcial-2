157ad553065c7864f78304921be83ecd6b4982f2
JhoxmanXD <93340770+JhoxmanXD@users.noreply.github.com>
2025-05-21 20:01:48 -0500

    Add files via upload
    Finalización del parcial con las especificaciones completas

--- Parcial/js/cart.js
+++ Parcial/js/cart.js
@@ -1,11 +1,9 @@
-function getProducts(page) {
-    document.getElementById('cardHeader').innerHTML = '<h4>Listado de productos</h4>'
-    document.getElementById('info').innerHTML = ''
-    fetch("https://reqres.in/api/unknown?page="+page, {
+function getCarts() {
+    document.getElementById('cardHeader').innerHTML = '<h4>Carrito de compras</h4>'
+    fetch("https://fakestoreapi.com/carts", {
         method: "GET",
         headers: {
             "Content-type": "application/json",
-            'x-api-key': 'reqres-free-v1'
         },
 
     })
@@ -19,69 +17,72 @@ function getProducts(page) {
                 }
             )
         })
-
-        .then((response => {
+        .then((response) => {
             if (response.status === 200) {
-                listProducts = `
-                <table class="table">
-                    <thead>
-                        <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">name</th>
-                        <th scope="col">year</th>
-                        <th scope="col">color</th>
-                        <th scope="col">pantone</th>
-                        <th scope="col">Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-            `
-                response.body.data.forEach(product => {
-                    listProducts = listProducts.concat(`
-                    <tr>
-                        <td>${product.id}</td>
-                        <td>${product.name}</td>
-                        <td>${product.year}</td>
-                        <td><input type="color" value="${product.color}"></td>
-                        <td>${product.pantone_value}</td>
-                        <td><button type="button" class="btn btn-outline-info" onclick="showInfoProducts('${product.id}')">View</button></td>
+                let listcarts = `
+              <table class="table">
+                  <thead>
+                      <tr>
+                      <th scope="col">#</th>
+                      <th scope="col">cart ID</th>
+                      <th scope="col">Product ID</th>
+                      <th scope="col">Quantity</th>
+                      </tr>
+                  </thead>
+                  <tbody>
+      
+          `
+                response.body.forEach(cart => {
+                    let prod1 = ""
+                    let prod2 = ""
+                    let prod3 = ""
+                    if (cart.products[0].productId !== null) {
+                        prod1 = cart.products[0].productId
+                    }
+                    if (cart.products.length > 1) {
+                        prod2 = ", " + cart.products[1].productId
+                    }
+                    if (cart.products.length > 2) {
+                        prod3 = ", " + cart.products[2].productId
+                    }
+                    let can1 = ""
+                    let can2 = ""
+                    let can3 = ""
+                    if (cart.products[0].productId !== null) {
+                        can1 = cart.products[0].quantity
+                    }
+                    if (cart.products.length > 1) {
+                        can2 = ", " + cart.products[1].quantity
+                    }
+                    if (cart.products.length > 2) {
+                        can3 = ", " + cart.products[2].quantity
+                    }
+                    listcarts = listcarts.concat(`
+                      <tr>
+                          <td>${cart.id}</td>
+                          <td>${cart.userId}</td>
+                          <td>${prod1}${prod2}${prod3}</td>
+                          <td>${can1}${can2}${can3}</td>
+                          <td><button type="button" class="btn btn-outline-info" onclick="showInfocart('${cart.id}')">View</button></td>
+                      </tr>
+                      `)
 
-                    </tr>
-                    `)
-                })
-                listProducts = listProducts.concat(`
-                </tbody>
-            </table>
-            <nav aria-label="Page navigation example">
-                  <ul class="pagination justify-content-center">
-                    <li class="page-item">
-                      <a class="page-link" href="#" aria-label="Previous">
-                        <span aria-hidden="true">&laquo;</span>
-                      </a>
-                    </li>
-                    <li class="page-item"><a class="page-link" href="#" onclick="getProducts('1')"">1</a></li>
-                    <li class="page-item"><a class="page-link" href="#" onclick="getProducts('2')">2</a></li>
-                    <li class="page-item">
-                      <a class="page-link" href="#" aria-label="Next">
-                        <span aria-hidden="true">&raquo;</span>
-                      </a>
-                    </li>
-                  </ul>
-                </nav>
-                `)
-
-                document.getElementById("info").innerHTML = listProducts
-
+                });
+                document.getElementById('info').innerHTML = listcarts
             }
-        }))
+            else {
+                document.getElementById('info').innerHTML = '<h3>No se encontraron productos en el carrito</h3>'
+            }
+        })
+
 
 }
-function showInfoProducts(productId) {
-    fetch("https://reqres.in/api/unknown/" + productId, {
+
+function showInfocart(cartId) {
+    fetch("https://fakestoreapi.com/carts/" + cartId, {
         method: "GET",
         headers: {
             "Content-type": "application/json",
-            'x-api-key': 'reqres-free-v1'
         },
     })
         .then((result) => {
@@ -97,7 +98,7 @@ function showInfoProducts(productId) {
 
         .then((response) => {
             if (response.status === 200) {
-                showModalProducts(response.body.data)
+                showModalcart(response.body)
             }
             else {
                 document.getElementById('info').innerHTML = '<h3>No se encontro el producto</h3>'
@@ -105,35 +106,57 @@ function showInfoProducts(productId) {
         })
 }
 
-function showModalProducts(product){
-    const modalProduct=`
-    <div class="modal fade" id="modalProduct" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-      <div class="modal-dialog modal-sm">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h1 class="modal-title fs-5" id="exampleModalLabel">Show Product</h1>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-          </div>
-          <div class="modal-body">
-            <div class="card">
-              <input class="card-img-top" type="color" value=${product.color}>
-              <div class="card-body">
-                <h5 class="card-title">Product Info</h5>
-                <p class="card-text">Name :${product.name}</p>
-                <p class="card-text">Year :${product.year}</p>
-                <p class="card-text">pantone value :${product.pantone_value}</p>
-              </div>
+function showModalcart(cart) {
+    let prod1 = ""
+    let prod2 = ""
+    let prod3 = ""
+    if (cart.products[0].productId !== null) {
+        prod1 = cart.products[0].productId
+    }
+    if (cart.products.length > 1) {
+        prod2 = ", " + cart.products[1].productId
+    }
+    if (cart.products.length > 2) {
+        prod3 = ", " + cart.products[2].productId
+    }
+    let can1 = ""
+    let can2 = ""
+    let can3 = ""
+    if (cart.products[0].productId !== null) {
+        can1 = cart.products[0].quantity
+    }
+    if (cart.products.length > 1) {
+        can2 = ", " + cart.products[1].quantity
+    }
+    if (cart.products.length > 2) {
+        can3 = ", " + cart.products[2].quantity
+    }
+    const modalcart = `
+  <div class="modal fade" id="modalcart" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-sm">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="exampleModalLabel">Show cart</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">cart Info</h5>
+              <p class="card-text">Id de usuario:${cart.userId}</p>
+              <p class="card-text">Producto :${prod1}${prod2}${prod3}</p>
+              <p class="card-text">Quantity :${can1}${can2}${can3}</p>
             </div>
           </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secundary" style="background-color:${product.color}" data-bs-dismiss="modal">Close</button>
-          </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
         </div>
       </div>
     </div>
-    `
-    document.getElementById('showModal').innerHTML=modalProduct
-    const modal=new bootstrap.Modal(document.getElementById('modalProduct'))
+  </div>
+  `
+    document.getElementById('showModal').innerHTML = modalcart
+    const modal = new bootstrap.Modal(document.getElementById('modalcart'))
     modal.show()
 }   
-
